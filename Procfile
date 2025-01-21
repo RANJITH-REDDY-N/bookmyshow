6a7543d90eead27359a750e93b20fc5e2f931464
RANJITH-REDDY-N <ranjeethnivas8@gmail.com>
2025-01-21 18:49:06 -0500

    Added Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar target/BookMyShow-0.0.1-SNAPSHOT.jar
--- src/main/java/com/bookmyshow/controllers/TicketController.java
+++ src/main/java/com/bookmyshow/controllers/TicketController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/tickets")
+@CrossOrigin(origins = "*")
 public class TicketController {
 
     @Autowired
